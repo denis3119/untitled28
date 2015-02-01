bc4c24f9b0ebd49d5b385c380452ab2c71b49c74
denis3119 <denis3119@gmail.com>
2015-02-01 15:44:46 +0300

    d

--- spring/src/main/java/denik/controller/HomeController.java
+++ spring/src/main/java/denik/controller/HomeController.java
@@ -1,5 +1,13 @@
 package denik.controller;
 
+<<<<<<< HEAD
+=======
+import Entity.Comment;
+import Entity.Publication;
+import Entity.User;
+import EntityServices.CommentService;
+import EntityServices.PublicationService;
+>>>>>>> parent of 066e827... Before spring
 import EntityServices.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +15,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
+<<<<<<< HEAD
 //		//TestTestTEst.main();
 //		UserService userService= new UserService();
 //		User user= new User();
@@ -40,6 +50,32 @@ public class HomeController {
 		UserService userService= new UserService();
 		userService.toList();
 
+=======
+		//TestTestTEst.main();
+		UserService userService= new UserService();
+		User user= new User();
+//		user.setName("name"+ new Random().nextInt());
+//		user.setPassword("password");
+//		user.setBlocked(false);
+//		user.setRole("ROLE_USER");
+//		userService.add(user);
+		Publication publication= new Publication();
+		publication.setCode("Code");
+		publication.setText("text");
+		publication.setTitle("title");
+		Comment comment = new Comment();
+		comment.setText("hello");
+//		userService.addPublication(3,publication);
+//		userService.deleteById(3);
+		List<User> list = userService.toList();
+		user= userService.getById(1);
+		PublicationService publicationService = new PublicationService();
+		CommentService commentService = new CommentService();
+		commentService.add(1,1,"hello");
+		List<Comment> commentList=	commentService.toList();
+		Comment comments= userService.toList().get(0).getComments().get(0);
+		String d;
+>>>>>>> parent of 066e827... Before spring
 		return new ModelAndView("home");
 	}
 
